68549c7836ff3122c1b4390138e6fa1d94d0ee64
Quentin <quentin.lemaire@eyesnap.fr>
2018-05-21 16:47:41 +0200

    bug fixe

--- src/train.py
+++ src/train.py
@@ -162,9 +162,9 @@ def train(resume):
         # save progress every 10000 iterations
         if t % 10000 == 0:
             saver.save(sess, 'pyth/' + "bird" + '-dqn', global_step=t)
-            np.save(Qs, "q.npz")
-            np.save(best_rewards, "best.npz")
-            np.save(rewards_game, "game_rewards.npz")
+            np.save(np.array(Qs), "q.npz")
+            np.save(np.array(best_rewards), "best.npz")
+            np.save(np.array(rewards_game), "game_rewards.npz")
 
 
         # print info
